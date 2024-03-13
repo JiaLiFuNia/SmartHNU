d5d1563b692e83b2ed86b5d19b0f49422172e3ba
JiaLiFuNia <woshixubohan04@163.com>
2024-03-13 11:31:38 +0800

    优化 新闻详情页WebView的字体以及背景颜色

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 33
         versionCode = 1
-        versionName = "2.0.3.10"
+        versionName = "2.0.3.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
