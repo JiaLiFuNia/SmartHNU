c175f9d15d2e9af92f29ae78ff013959338878f4
Jialifuniya <woshixubohan04@163.com>
2024-07-24 23:47:07 +0800

    新增 消息中心已读请求 新增 教学评价可切换学期 新增 登录请求指示器 优化 部分文字表述 优化 字体颜色 修复 选定教材下拉刷新时请求错误的问题 修复 已知问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 29
         targetSdk = 33
         versionCode = 1
-        versionName = "2.1.0_beta.24"
+        versionName = "2.1.0_beta.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
