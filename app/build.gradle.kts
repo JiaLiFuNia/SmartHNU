976ab1a0111deebcd01b5b0ca1c02a5ee2c5be8b
Jialifuniya <woshixubohan04@163.com>
2024-08-18 16:55:21 +0800

    新增 容易闪退的bug 修复 选定教材和上课任务无法切换学期的问题 修复 消息中心闪退的问题 修复 新闻搜索页面频繁刷新的问题 修复 网络检测页面，深色模式下颜色异常

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 33
         versionCode = 1
-        versionName = "2.1.9.6"
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
