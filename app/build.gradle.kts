30f8f525b79a465597146baa66482ea727839b18
Jialifuniya <woshixubohan04@163.com>
2024-09-09 13:07:12 +0800

    新增 容易闪退的bug 新增 长按个人信息卡牌可修改头像 新增 部分页面适配横屏 修复 微信公众号文章无法加载的问题 修复 选定教材和上课任务无法切换学期的问题 修复 消息中心闪退的问题 修复 新闻搜索页面频繁刷新的问题 优化 网络检测页面为吐司提示 优化 设置页面 优化 新闻详情预览，对于htu.edu.cn新闻直接提取正文，其他来源直接浏览原页面

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 33
         versionCode = 1
-        versionName = "2.2.7"
+        versionName = "2.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
