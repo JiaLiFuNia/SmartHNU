4960710832bde8c80ef6ddea04ffe211cfe49f58
Jialifuniya <woshixubohan04@163.com>
2025-03-05 12:50:30 +0800

    改了很多东西

--- app/src/main/java/com/smart/htu/utils/GenerateQWeather.kt
+++ app/src/main/java/com/smart/htu/utils/GenerateQWeather.kt
@@ -21,7 +21,7 @@ object GenerateQWeather {
     //用于生成和风天气密钥
     fun getQWeatherAuth(): String {
         return "Bearer " + generateJWT(
-            privateKeyStr = RSAUtil.getPrivateKeyFromRaw(context, R.raw.private_key),
+            privateKeyStr = "MC4CAQAwBQYDK2VwBCIEILLPuzDVvJ0tIE1/2wiDwUSwZr2Lwt/BaOxJ7sYaIPrk",
             projectID = "2AKUWC7K78",
             keyID = "CHPN45DFAX"
         )
