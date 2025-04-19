7955ee3f79f12232f12cfc00432d227791867827
Jialifuniya <woshixubohan04@163.com>
2025-04-19 20:35:57 +0800

    Update version code and enhance Telegram bot file upload handling in CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.smart.htu"
         minSdk = 29
         targetSdk = 35
-        versionCode = 202504061
+        versionCode = 202504191
         versionName = "3.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
