959fb234f20ccceeb52a892cdb30a63c466be959
Jialifuniya <woshixubohan04@163.com>
2025-04-21 23:57:54 +0800

    feat: Refactor UI components to use LazyColumn for better performance and layout consistency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.smart.htu"
         minSdk = 29
         targetSdk = 35
-        versionCode = 202504211
+        versionCode = 202504212
         versionName = "3.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
