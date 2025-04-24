75e0310b195645cd359413f1457388912515dc70
Jialifuniya <woshixubohan04@163.com>
2025-04-24 13:01:43 +0800

    feat: Bump versionCode to 202504241 for upcoming release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.smart.htu"
         minSdk = 29
         targetSdk = 35
-        versionCode = 202504231
+        versionCode = 202504241
         versionName = "3.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
