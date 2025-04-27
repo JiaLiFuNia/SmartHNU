0a4d650f67ac841f1bd864cfbb1e08b497836323
Jialifuniya <woshixubohan04@163.com>
2025-04-27 23:17:55 +0800

    fix: AboutLibraries 在 material 版本大于 1.8.0 时闪退的问题

---
+++ app/src/main/java/com/smart/htu/component/aboutLibrary/AboutLibraries.kt
@@ -0,0 +1,92 @@
+package com.smart.htu.component.aboutLibrary
+
+import android.content.Context
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.fromHtml
+import androidx.compose.ui.unit.dp
+import com.mikepenz.aboutlibraries.Libs
+import com.mikepenz.aboutlibraries.entity.Library
+import com.mikepenz.aboutlibraries.ui.compose.LibraryColors
+import com.mikepenz.aboutlibraries.ui.compose.LibraryDefaults
+import com.mikepenz.aboutlibraries.ui.compose.LibraryDimensions
+import com.mikepenz.aboutlibraries.ui.compose.LibraryPadding
+import com.mikepenz.aboutlibraries.ui.compose.LibraryTextStyles
+import com.mikepenz.aboutlibraries.ui.compose.m3.libraryColors
+import com.mikepenz.aboutlibraries.ui.compose.util.htmlReadyLicenseContent
+import com.mikepenz.aboutlibraries.util.withContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Displays all provided libraries in a simple list.
+ */
+@Composable
+fun LibrariesContainer(
+    modifier: Modifier = Modifier,
+    librariesBlock: (Context) -> Libs = { context ->
+        Libs.Builder().withContext(context).build()
+    },
+    lazyListState: LazyListState = rememberLazyListState(),
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    showAuthor: Boolean = true,
+    showDescription: Boolean = false,
+    showVersion: Boolean = true,
+    showLicenseBadges: Boolean = true,
+    colors: LibraryColors = LibraryDefaults.libraryColors(),
+    padding: LibraryPadding = LibraryDefaults.libraryPadding(),
+    dimensions: LibraryDimensions = LibraryDefaults.libraryDimensions(),
+    textStyles: LibraryTextStyles = LibraryDefaults.libraryTextStyles(),
+    header: (LazyListScope.() -> Unit)? = null,
+    divider: (@Composable LazyItemScope.() -> Unit)? = null,
+    footer: (LazyListScope.() -> Unit)? = null,
+    onLibraryClick: ((Library) -> Unit)? = null,
+) {
+    val context = LocalContext.current
+
+    val libraries = produceState<Libs?>(null) {
+        value = withContext(Dispatchers.IO) {
+            librariesBlock(context)
+        }
+    }
+    LibrariesContainer(
+        libraries = libraries.value,
+        modifier = modifier,
+        lazyListState = lazyListState,
+        contentPadding = contentPadding,
+        showAuthor = showAuthor,
+        showDescription = showDescription,
+        showVersion = showVersion,
+        showLicenseBadges = showLicenseBadges,
+        colors = colors,
+        padding = padding,
+        dimensions = dimensions,
+        textStyles = textStyles,
+        header = header,
+        divider = divider,
+        footer = footer,
+        onLibraryClick = onLibraryClick,
+        licenseDialogBody = { library ->
+            val license = remember(library) {
+                library.htmlReadyLicenseContent.takeIf { it.isNotEmpty() }
+                    ?.let { AnnotatedString.fromHtml(it) }
+            }
+            if (license != null) {
+                Text(
+                    text = license,
+                    color = colors.contentColor
+                )
+            }
+        }
+    )
+}
