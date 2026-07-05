d37abf9189933e77eb3cc23d04c0bb4d0b5218e4
JiaLiFuNia <xbh0704@outlook.com>
2026-07-05 12:45:01 +0800

    refactor: remove Material3 dependency and update MiuiX version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,6 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.core)
     implementation(libs.androidx.material.icons.extended)
 
